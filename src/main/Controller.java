e9a03946e1e1a12958e02344c40c652d9dedc97e
dbwls <youjin9874@naver.com>
2021-09-13 10:52:59 +0900

    메인플젝생성

---
+++ src/main/Controller.java
@@ -0,0 +1,18 @@
+package main;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+
+public class Controller implements Initializable{
+	Parent root;
+	public void setRoot(Parent root) {
+		this.root = root;
+		}
+
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+}
+}
